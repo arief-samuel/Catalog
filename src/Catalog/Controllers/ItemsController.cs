2aa23cf655ab6b439748c60b031531c8e9adff51
arief_el <ariefsamuel21@gmail.com>
2021-06-24 22:09:14 +0700

    Init

---
+++ src/Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+
+
+        private readonly ILogger<ItemsController> _logger;
+        private InMemItemRepository items;
+        public ItemsController(ILogger<ItemsController> logger)
+        {
+            _logger = logger;
+            items = new InMemItemRepository();
+        }
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return items.GetItem();
+        }
+        [HttpGet("{Id}")]
+        public Item GetItem(Guid Id)
+        {
+            return items.GetItem(Id);
+        }
+    }
+}
---
+++ src/Catalog/Repositories/InMemItemRepository.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 10, CreatedDate = DateTimeOffset.Now },
+            new Item { Id = Guid.NewGuid(), Name = "Sword", Price = 30, CreatedDate = DateTimeOffset.Now },
+            new Item { Id = Guid.NewGuid(), Name = "Medicine", Price = 5, CreatedDate = DateTimeOffset.Now },
+            new Item { Id = Guid.NewGuid(), Name = "Guard", Price = 8, CreatedDate = DateTimeOffset.Now },
+            new Item { Id = Guid.NewGuid(), Name = "Knife", Price = 4, CreatedDate = DateTimeOffset.Now },
+        };
+
+        public Item GetItem(Guid Id)
+        {
+            return items.Where(x => x.Id == Id).SingleOrDefault();
+        }
+
+        public IEnumerable<Item> GetItem()
+        {
+            return items;
+        }
+    }
+}
